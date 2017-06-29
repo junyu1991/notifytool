5805b89622a7406e4ef31f6f5c4a589b014a5d25
junyu1991 <juny1991@163.com>
2017-06-29 17:34:29 +0800

    finish test.py

--- test.py
+++ test.py
@@ -2,8 +2,9 @@
 #!encoding:utf-8
 #date:2017-05-08
 
-from lib import tools
+#from lib import tools
 from lib import notify_tool
+import os
 
 def test():
     #result=tools.get_target_files('/home/yujun/work/python/mytool/notify/config/target_file.xml')
@@ -14,4 +15,9 @@ def test():
 
 
 if __name__=='__main__':
-    notify_tool.test()
+    if os.fork():
+        print os.getpid()
+        notify_tool.tomcat_monitor()
+        notify_tool.system_monitor()
+    #import sys
+    #sys.exit(1)
